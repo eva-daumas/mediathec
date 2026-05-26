5ff5310b06532567a4025562cfaa977b7921758d
eva-daumas <eva.daumas@laplateforme.io>
2026-05-26 16:10:10 +0200

    Architecture en cours du project mediathec

---
+++ member-service/src/main/java/com/mediathec/memberservice/MemberServiceApplication.java
@@ -0,0 +1,15 @@
+package com.mediathec.memberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class MemberServiceApplication {
+
+    public static void main(String[] args) {
+        run(MemberServiceApplication.class, args);
+    }
+
+}
