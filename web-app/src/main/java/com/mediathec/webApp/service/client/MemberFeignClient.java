015cceddc95803e871f92db0178b2ab9bafe1d7a
eva-daumas <eva.daumas@laplateforme.io>
2026-06-17 14:43:04 +0200

    Modifs dans web-app et members-service

--- web-app/src/main/java/com/mediathec/webApp/service/client/MemberFeignClient.java
+++ web-app/src/main/java/com/mediathec/webApp/service/client/MemberFeignClient.java
@@ -9,8 +9,9 @@ import java.util.List;
 @FeignClient(name = "members-service", url = "http://localhost:8085")
 public interface MemberFeignClient {
 
-    @GetMapping("/api/members/email/{email}")
+    @GetMapping("/api/members/email/{email}")  // ← Cette URL
     Member getMemberByEmail(@PathVariable("email") String email);
+
 
     @PostMapping("/add")
     Member createMember(@RequestBody Member member);
